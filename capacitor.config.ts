2e9fb60fb95404d8d39a7d764cdd1798b33b8e03
miloscabrilo <mlscabrilo5@gmail.com>
2023-03-02 22:22:14 +0100

    Created Capacitor Android project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.warehouse.mcabrilo',
+  appName: 'warehouse',
+  webDir: 'dist/warehouse',
+  bundledWebRuntime: false
+};
+
+export default config;
